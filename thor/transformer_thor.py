5d249e2af03806bbfdf0b886d13220dd3d2d70bb
Xiaodong Liu <xiaodl@microsoft.com>
2021-10-07 14:16:58 -0700

    copy right

--- thor/transformer_thor.py
+++ thor/transformer_thor.py
@@ -1,3 +1,8 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 import torch
 
 from torch import Tensor
